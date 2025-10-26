f1102c9a00b2135262dc906a561f2df5eb4f3deb
victorsena19 <victor_sena19@hotmail.com>
2025-10-26 15:23:27 -0400

    first commit

---
+++ src/main/java/br/com/sistema_contas/SistemaContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistema_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sistema_contas/SistemaContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistema_contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
